0dceda8394c7477cb7b3fe17d5ceb1d1db69d2df
aidanslator <aslator@ualberta.ca>
2026-01-11 13:05:41 -0700

    calMatrixBlock done

--- main.c
+++ main.c
@@ -3,25 +3,43 @@
 
 int **A, **B, **C;
 int n;
-double time;
+int p;
 
-int** calMatrixBlock(int threads){
+int** calMatrixBlock(int thread){
+
+    int threadX = floor(thread / n);
+    int threadY = thread % n;
+    //ADD ERROR CHECKING
+    int blockWidth = n/sqrt(p);
+    int sum;
+
+    for(int i = 0; i < blockWidth; ++i){
+        for(int j = 0; j<blockWidth; ++j){
+            sum = 0;
+            for(int k = 0; k<n; ++k){
+                sum+=A[i+threadX*blockWidth][k]*B[k][j+threadY*blockWidth];
+            }
+
+            C[i+threadX*blockWidth][threadY*blockWidth] = sum;
+
+        }
+    }
 
 }
 
 int main (int argc, char* argv[]){
 
-    int numOfThreads = atoi(argv[1]);
-    pthread_t* threadHandles = malloc(numOfThreads*sizeof(pthread_t)); 
-    int numberOfBlocks = sqrt(numOfThreads);
+    p = atoi(argv[1]);
+    pthread_t* threadHandles = malloc(p*sizeof(pthread_t)); 
+    int numberOfBlocks = sqrt(p);
 
     Lab1_loadinput(&A, &B, &n);
 
     for(int i = 0; i++; )
 
-    int numberOfBlocks = sqrt(numOfThreads);
+    int numberOfBlocks = sqrt(p);
 
-    for (int thread = 0; thread < numOfThreads; thread++) {
+    for (int thread = 0; thread < p; thread++) {
         pthread_create(&threadHandles[thread], NULL, calMatrixBlock, (void*)thread);
     }
 
